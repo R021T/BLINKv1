dbfe2bf275a71c06b1d9ec2d0aa012fe094d865b
Rohit C <124383636+R021T@users.noreply.github.com>
2023-12-13 02:42:49 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,18 @@
- echo "BUILD START"
- python3.9 -m venv venv
- source venv\Scripts\activate
- pip install --upgrade pip
- pip install -r requirements.txt
- echo "BUILD END"
+echo "BUILD START"
+
+# Set MARIADB_CONFIG environment variable
+export MARIADB_CONFIG="C:\\Program Files\\MariaDB 11.2\\bin\\mysql_config.pl"
+
+# Create virtual environment
+python -m venv venv
+
+# Activate virtual environment
+venv\Scripts\activate
+
+# Upgrade pip
+python -m pip install --upgrade pip
+
+# Install dependencies
+python -m pip install -r requirements.txt
+
+echo "BUILD END"
