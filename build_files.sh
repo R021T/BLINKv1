b21905bd470e4577e68a0784bacbaf8c4bc43ca4
Rohit C <124383636+R021T@users.noreply.github.com>
2023-12-13 02:45:36 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,18 @@
 echo "BUILD START"
 
-# Set MARIADB_CONFIG environment variable
-export MARIADB_CONFIG="C:\\Program Files\\MariaDB 11.2\\bin\\mysql_config.pl"
-
 # Create virtual environment
-python -m venv venv
+python3.9 -m venv venv
 
 # Activate virtual environment
 venv\Scripts\activate
 
 # Upgrade pip
-python -m pip install --upgrade pip
+pip install --upgrade pip
 
 # Install dependencies
-python -m pip install -r requirements.txt
+pip install --disable-pip-version-check --target . --upgrade -r /vercel/path0/requirements.txt
+
+# Set MARIADB_CONFIG environment variable
+export MARIADB_CONFIG="C:\Program Files\MariaDB 11.2\bin\mysql_config.pl"  # Replace with the correct path
 
 echo "BUILD END"
