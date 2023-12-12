897a9a422a23631c02be9a603cc004933f0f5ba2
Rohit C <124383636+R021T@users.noreply.github.com>
2023-12-13 02:11:17 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
  echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
+ python3.9 -m venv venv
+ source venv\Scripts\activate
+ pip install --upgrade pip
+ pip install -r requirements.txt
  echo "BUILD END"
