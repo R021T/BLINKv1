dbf3275848f387a392160f64845814fd4ff3223b
Rohit C <124383636+R021T@users.noreply.github.com>
2023-12-13 19:11:49 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -12,7 +12,4 @@ pip install --upgrade pip
 # Install dependencies
 pip install --disable-pip-version-check --target . --upgrade -r /vercel/path0/requirements.txt
 
-# Set MARIADB_CONFIG environment variable
-export MARIADB_CONFIG="\mariadb_config.pl"  # Replace with the correct path
-
 echo "BUILD END"
