641f05f2204fbf43f8104e5330d680b42cb5a4c9
Rohit C <124383636+R021T@users.noreply.github.com>
2023-12-16 17:54:16 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,24 +1,4 @@
-#!/bin/bash
-
-echo "BUILD START"
-
-# Install system dependencies
-sudo apt-get update
-sudo apt-get install -y python3.9 python3.9-venv cmake
-
-# Create virtual environment
-python3.9 -m venv venv
-
-# Activate virtual environment
-source venv/bin/activate
-
-# Upgrade pip
-pip install --upgrade pip
-
-# Install dependencies
-pip install --disable-pip-version-check --target . --upgrade -r /vercel/path0/requirements.txt
-
-# Run Django migrations (assuming your project has database migrations)
-python manage.py migrate
-
-echo "BUILD END"
+ echo "BUILD START"
+ python3.9 -m pip install -r requirements.txt
+ python3.9 manage.py collectstatic --noinput --clear
+ echo "BUILD END"
