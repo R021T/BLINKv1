220f022c3ed17a3b57136501e81386d8ffb1ef3d
Rohit C <124383636+R021T@users.noreply.github.com>
2023-12-17 16:23:19 +0530

    Update post-build.sh

--- post-build.sh
+++ post-build.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Set CMake binary URL
+export CMAKE_BINARY_URL="https://cmake.org/files/v3.22/cmake-3.22.1-Linux-x86_64.sh"
+
 # Download and install CMake
 curl -L -o cmake.sh $CMAKE_BINARY_URL
 chmod +x cmake.sh
