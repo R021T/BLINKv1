b041c29f090ea38a67d3e0d731f0955d74c889a0
Rohit C <124383636+R021T@users.noreply.github.com>
2023-12-17 17:37:48 +0530

    Delete blink directory

--- blink/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns={
-    path('',views.home),
-    path('start',views.start),
-    path('stop',views.stop),
-    path('clear',views.clear)
-    }
